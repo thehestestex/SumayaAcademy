716affa48faa823993e2735b87647437b2960573
thehestestex <138957007+thehestestex@users.noreply.github.com>
2023-09-23 21:41:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import requests
 from pytz import timezone
 from datetime import datetime
 import json
+from fastapi.responses import HTMLResponse
 from fastapi.responses import FileResponse
 
 import json
@@ -20,3 +21,41 @@ async def verif():
     allin = await retall()
     json_data = json.dumps(allin)
     return json_data
+
+@kalwar.get("/" ,   response_class=HTMLResponse)
+async def rootu():
+    html_content = """
+     <html>
+         <head>
+             <title>Sumaya Academy</title>
+         </head>
+         <body>
+         
+             <h1>Developed By Jatin Kalwar</h1>
+             <br>
+             <h3>For Business Query Contact us on  thejatinkalwar@gmail.com</h3>
+         </body>
+     </html>
+     """
+    return HTMLResponse(content=html_content, status_code=200)
+
+@kalwar.get("/sumayaacademy/login/" ,  response_class=PlainTextResponse)
+async def loginf(userid , password , deviceid):
+    checkuser = await  userexists(userid)
+    if checkuser=="exists":
+        pass
+    else:
+        return "nouserfound"
+    # checkpass = await userloginpass(password)
+    if password=="sumaya@123":
+        pass
+    else:
+        return "invalid password"
+    deviceidreturn = await getdeviceid(userid)
+    gi = await getdeviceidall(deviceid)
+    if gi=="true":
+        return gi
+    if deviceidreturn=="None" or deviceidreturn=="null":
+        return await newdeviceid(userid , deviceid)
+    else:
+        return "already logged in"
