b8836920c8dfc885173ddd490620041baca2c4db
thehestestex <138957007+thehestestex@users.noreply.github.com>
2023-09-27 14:35:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from backend import *
-from fastapi import FastAPI , Request , BackgroundTasks
+from fastapi import FastAPI, Request, BackgroundTasks
 from fastapi.responses import HTMLResponse
 from fastapi.responses import PlainTextResponse
 import requests
@@ -10,19 +10,22 @@ from fastapi.responses import HTMLResponse
 from fastapi.responses import FileResponse
 
 import json
-kalwar = FastAPI(docs_url=None , redoc_url=None , openapi_url=None)
+
+kalwar = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+
 @kalwar.get("/sumayaacademy/keys")
 async def verify():
     ke = await returnseckey()
     return ke
 
-@kalwar.get("/sumayaacademy/data" ,  response_class=PlainTextResponse)
+
+@kalwar.get("/sumayaacademy/data", response_class=PlainTextResponse)
 async def verif():
     allin = await retall()
     json_data = json.dumps(allin)
     return json_data
 
-@kalwar.get("/" ,   response_class=HTMLResponse)
+@kalwar.get("/", response_class=HTMLResponse)
 async def rootu():
     html_content = """
      <html>
@@ -30,7 +33,7 @@ async def rootu():
              <title>Sumaya Academy</title>
          </head>
          <body>
-         
+
              <h1>Developed By Jatin Kalwar</h1>
              <br>
              <h3>For Business Query Contact me on  thejatinkalwar@gmail.com</h3>
@@ -39,28 +42,28 @@ async def rootu():
      """
     return HTMLResponse(content=html_content, status_code=200)
 
-@kalwar.get("/sumayaacademy/login/" ,  response_class=PlainTextResponse)
-async def loginf(userid , password , deviceid):
+@kalwar.get("/sumayaacademy/login/", response_class=PlainTextResponse)
+async def loginf(userid, password, deviceid):
     checkuser = await  userexists(userid)
-    if checkuser=="exists":
+    if checkuser == "exists":
         pass
     else:
         return "nouserfound"
     # checkpass = await userloginpass(password)
-    if password=="sumaya@123":
+    if password == "sumaya@123":
         pass
     else:
         return "invalid password"
     deviceidreturn = await getdeviceid(userid)
     dataa = await returndata(userid)
     gi = await getdeviceidall(deviceid)
-    if gi==userid:
+    if gi == userid:
         json_data = json.dumps(dataa)
         return json_data
 
-    if deviceidreturn=="None" or deviceidreturn=="null":
-        gui = await newdeviceid(userid , deviceid)
-        if gui=="true":
+    if deviceidreturn == "None" or deviceidreturn == "null":
+        gui = await newdeviceid(userid, deviceid)
+        if gui == "true":
             json_data = json.dumps(dataa)
             return json_data
     else:
@@ -68,4 +71,14 @@ async def loginf(userid , password , deviceid):
 
 @kalwar.get("/sumayaacademy/addstudent/", response_class=PlainTextResponse)
 async def addstu(stuid, name, sem):
-    return await insertstu(stuid , sem , name)
+    return await insertstu(stuid, sem, name)
+
+@kalwar.get("/sumayaacademy/update/", response_class=PlainTextResponse)
+async def loginf( userid ,mobileno , gender):
+    dics = await updateinfo(userid , mobileno , gender)
+    json_data = json.dumps(dics)
+    return json_data
+
+@kalwar.get("/sumayaacademy/checkstatus/", response_class=PlainTextResponse)
+async def checkstatuss(userid):
+    return await getuserall(userid)
