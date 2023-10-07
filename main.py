d74cfa8dc28c2d0550aeea381063ca0c788c2df6
thehestestex <138957007+thehestestex@users.noreply.github.com>
2023-10-07 18:09:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 from backend import *
-from fastapi import FastAPI, Request, BackgroundTasks
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, BackgroundTasks, UploadFile, File, Form
 from fastapi.responses import PlainTextResponse
 import requests
-from pytz import timezone
-from datetime import datetime
-import json
+from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
-from fastapi.responses import FileResponse
+
 
 import json
 
-kalwar = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+kalwar = FastAPI()
 
 @kalwar.get("/sumayaacademy/keys")
 async def verify():
     ke = await returnseckey()
     return ke
-
-def startauto():
-    requests.get("https://autourl-3ptn.onrender.com/start")
 
 
 @kalwar.get("/sumayaacademy/data", response_class=PlainTextResponse)
@@ -29,8 +23,7 @@ async def verif():
     return json_data
 
 @kalwar.get("/", response_class=HTMLResponse)
-async def rootu(background_tasks: BackgroundTasks):
-    background_tasks.add_task(startauto)
+async def rootu():
     html_content = """
      <html>
          <head>
@@ -80,12 +73,21 @@ async def addstu(stuid, name, sem):
 @kalwar.get("/sumayaacademy/update/", response_class=PlainTextResponse)
 async def loginf( userid ,mobileno , gender):
     dics = await updateinfo(userid , mobileno , gender)
-    if dics=="false":
-        return "false"
-    else:
-        json_data = json.dumps(dics)
-        return json_data
+    json_data = json.dumps(dics)
+    return json_data
 
 @kalwar.get("/sumayaacademy/checkstatus/", response_class=PlainTextResponse)
 async def checkstatuss(userid):
     return await getuserall(userid)
+
+@kalwar.post("/sumayaacademy/")
+async def uploadd(assign_id: str = Form(...) , file:UploadFile = File(...) ):
+
+        file_ext = file.filename.split(".").pop()
+        file_name = 21045
+        file_path = f"{file_name}.{file_ext}"
+        print(assign_id)
+        with open (file_path , 'wb') as f:
+            conent = await file.read();
+            f.write(conent)
+        return "ok"
